fabef1ca43da78a1405014b7cd03482bdfa6ad5d
Tlee <thienle255@gmail.com>
2024-04-26 20:14:13 +0700

    string

---
+++ src/_1491_Average_Salary_Excluding_the_Minimum_and_Maximum_Salary.java
@@ -0,0 +1,9 @@
+public class _1491_Average_Salary_Excluding_the_Minimum_and_Maximum_Salary {
+//    public static double average(int[] salary) {
+//
+//    }
+
+    public static void main(String[] args) {
+
+    }
+}
