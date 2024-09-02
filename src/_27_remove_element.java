778a033e8e2380d21fd0e5f05578cd9e341ccebd
Tlee <thienle255@gmail.com>
2024-09-02 17:37:09 +0700

    27 Remove Element

--- src/_27_remove_element.java
+++ src/_27_remove_element.java
@@ -1,17 +1,19 @@
 public class _27_remove_element {
 
     public static void main(String[] args) {
-        int[] a = {3, 4, 1, 3, 3, 2, 3};
+        int[] a = {1, 2, 3, 3, 4};
         System.out.println(removeElement(a, 3));
     }
-    private static int removeElement(int[] a, int x) {
+
+    private static int removeElement(int[] nums, int val) {
         int k = 0;
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] != x) {
-                a[k] = a[i];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
                 k++;
             }
         }
+
         return k;
     }
 }
