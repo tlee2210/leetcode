27f862bce04d4d26b55286d890895ba131c0f3cf
Tlee <thienle255@gmail.com>
2025-02-01 22:13:30 +0700

    27. Remove Element

--- src/_88_Merge_Sorted_Array.java
+++ src/_88_Merge_Sorted_Array.java
@@ -1,33 +1,32 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class _88_Merge_Sorted_Array {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        for (int a : nums2) {
-            chen(a, nums1, m);
-            m++;
-        }
-    }
-
-
-    public static void chen(int a, int[] nums1, int m) {
-        boolean check = false;
-
-        for (int i = 0; i < m; i++) {
-            if (nums1[i] > a) {
-                check = true;
-                for (int j = m - 1; j >= i; j--) {
-                    nums1[j + 1] = nums1[j];
-                }
-                nums1[i] = a;
-                break;
+        int i = m - 1;
+        int j = n - 1;
+        int k = (m + n) - 1;
+        while (k >= 0) {
+            if (i < 0) {
+                nums1[k] = nums2[j];
+                j--;
+            } else if (j < 0) {
+                nums1[k] = nums1[i];
+                i--;
+            } else if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
             }
-        }
-
-        if (!check) {
-            nums1[m] = a;
+            k--;
         }
     }
 
     public static void main(String[] args) {
         int[] num1 = {1, 2, 3, 0, 0, 0}, num2 = {2, 5, 6};
         merge(num1, 3, num2, 3);
+        System.out.println(Arrays.toString(num1));
     }
 }
