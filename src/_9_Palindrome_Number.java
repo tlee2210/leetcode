ed8492401480296f7f85cbe1debcb7ae846c40a2
Tlee <thienle255@gmail.com>
2025-02-16 23:21:02 +0700

    9 Palindrome Number

---
+++ src/_9_Palindrome_Number.java
@@ -0,0 +1,17 @@
+public class _9_Palindrome_Number {
+    public static boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
+        int num = 0;
+        int y = x;
+        while (y > 0) {
+            num = num * 10 + y % 10;
+            y /= 10;
+        }
+        return num == x;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+    }
+}
