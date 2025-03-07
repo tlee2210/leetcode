3f1278c8b6c58418f30b41eb3f91bee452b28836
Tlee <thienle255@gmail.com>
2025-03-07 23:31:14 +0700

    121. Best Time to Buy and Sell Stock

---
+++ src/_121_Best_Time_to_Buy_and_Sell_Stock.java
@@ -0,0 +1,21 @@
+public class _121_Best_Time_to_Buy_and_Sell_Stock {
+    public static int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit(prices));
+    }
+}
