dccd7c73aff2817753a00f92650fa12f5a71a374
Tlee <thienle255@gmail.com>
2025-03-09 23:31:41 +0700

    14. Longest Common Prefix

---
+++ src/_14_Longest_Common_Prefix.java
@@ -0,0 +1,18 @@
+public class _14_Longest_Common_Prefix {
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0)
+            return "";
+        String prefix = strs[0];
+        for (int i = 0; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+        }
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"flower", "flow", "flight"};
+        System.out.println(longestCommonPrefix(strs));
+    }
+}
