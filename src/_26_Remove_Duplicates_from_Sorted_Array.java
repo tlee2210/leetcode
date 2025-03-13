cc6b3969d69d8e565089cd469c44d54619cce514
Tlee <thienle255@gmail.com>
2025-03-13 23:29:51 +0700

    26. Remove Duplicates from Sorted Array

---
+++ src/_26_Remove_Duplicates_from_Sorted_Array.java
@@ -0,0 +1,11 @@
+public class _26_Remove_Duplicates_from_Sorted_Array {
+    public int removeDuplicates(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != nums[j]) {
+                nums[++j] = nums[i];
+            }
+        }
+        return j + 1;
+    }
+}
