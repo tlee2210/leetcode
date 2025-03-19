5938ad88bbab0faee3f23295f336c94ea08cdba2
Tlee <thienle255@gmail.com>
2025-03-19 20:45:10 +0700

    Sort

--- src/SortingAlgorithm.java
+++ src/SortingAlgorithm.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class SortingAlgorithm {
     public static void bubbleSort(int[] a) {
         int n = a.length;
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (a[j] > a[j + 1]) {
--- src/insertionSort.java
+++ src/insertionSort.java
@@ -1,12 +1,13 @@
+import java.util.Arrays;
+
 public class insertionSort {
 
     public static void insertionSort(int[] a) {
         int n = a.length;
 
-        for (int i = 1; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             int ai = a[i];
             int j = i - 1;
-
             while (j >= 0 && a[j] > ai) {
                 a[j + 1] = a[j];
                 j--;
@@ -14,11 +15,11 @@ public class insertionSort {
 
             a[j + 1] = ai;
         }
-
     }
 
     public static void main(String[] args) {
         int[] a = {5, 3, 2, 7, 8, 1};
         insertionSort(a);
+        System.out.println(Arrays.toString(a));
     }
 }
