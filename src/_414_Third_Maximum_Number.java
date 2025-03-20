265039b6728b37c1002c793504f79e4cf2a2d8ac
Tlee <thienle255@gmail.com>
2025-03-20 23:52:48 +0700

    _414_Third_Maximum_Number

--- src/_414_Third_Maximum_Number.java
+++ src/_414_Third_Maximum_Number.java
@@ -1,2 +1,43 @@
 public class _414_Third_Maximum_Number {
+
+    public static void insert(long[] maxArr, int val) {
+        int i = 0;
+
+        while (i < maxArr.length) {
+            if (val == maxArr[i]) {
+                return;
+            } else if (val > maxArr[i]) {
+                break;
+            } else {
+                i++;
+            }
+        }
+
+        if (i < maxArr.length) {
+            for (int j = maxArr.length - 2; j >= i; j--) {
+                maxArr[j + 1] = maxArr[j];
+            }
+            maxArr[i] = val;
+        }
+
+    }
+
+    public static int thirdMax(int[] a) {
+        long[] maxArr = {Long.MIN_VALUE, Long.MIN_VALUE, Long.MIN_VALUE};
+
+
+        for (int i = 0; i < a.length; i++) {
+            insert(maxArr, a[i]);
+        }
+
+        if (maxArr[2] == Long.MIN_VALUE) return (int) maxArr[0];
+
+        return (int) maxArr[2];
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 2, 3, 1};
+        System.out.println(thirdMax(a));
+
+    }
 }
