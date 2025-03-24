83febc486f7d7c42719c6799e3751537d4e5ee36
Tlee <thienle255@gmail.com>
2025-03-24 22:46:41 +0700

    _217_Contains_Duplicate

---
+++ src/_217_Contains_Duplicate.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+public class _217_Contains_Duplicate {
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        System.out.println(containsDuplicate(nums));
+    }
+}
