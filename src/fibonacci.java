6b6b9bfb1787a69379f50a143c1d69c92c4e8804
Tlee <thienle255@gmail.com>
2025-04-01 22:22:02 +0700

    Fibo

--- src/fibonacci.java
+++ src/fibonacci.java
@@ -6,7 +6,25 @@ public class fibonacci {
         return Fibo(n - 1) + Fibo(n - 2);
     }
 
-    public static void main(String[] args) {
+    static int[] F = new int[1000];
 
+    public static int Fibo1(int n) {
+        if (F[n] != 0) return F[n];
+
+
+        if (n <= 2) return 1;
+
+        F[n] = Fibo(n - 1) + Fibo(n - 2);
+
+        return F[n];
+    }
+
+    public static void main(String[] args) {
+        int[] Fibo = new int[11];
+        Fibo[1] = 1;
+        Fibo[2] = 1;
+        for (int i = 3; i < 10; i++) {
+            Fibo[i] = Fibo[i - 1] + Fibo[i - 2];
+        }
     }
 }
