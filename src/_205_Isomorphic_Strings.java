47ed3b4c02bd0125ac1ce46cbc5461a994edf771
Tlee <thienle255@gmail.com>
2025-04-13 23:27:41 +0700

    _205_Isomorphic_Strings

---
+++ src/_205_Isomorphic_Strings.java
@@ -0,0 +1,21 @@
+public class _205_Isomorphic_Strings {
+    public static boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) return false;
+        int[] m1 = new int[256];
+        int[] m2 = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            if (m1[s.charAt(i)] != m2[t.charAt(i)]) return false;
+
+            m1[s.charAt(i)] = i + 1;
+            m2[t.charAt(i)] = i + 1;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "egg", t = "add";
+
+        System.out.println(isIsomorphic(s, t));
+    }
+}
