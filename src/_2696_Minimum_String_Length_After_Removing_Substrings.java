13a63196e947845bcc4e67e043ac70f3480128f9
Tlee <thienle255@gmail.com>
2025-04-24 23:17:23 +0700

    _2696_Minimum_String_Length_After_Removing_Substrings

---
+++ src/_2696_Minimum_String_Length_After_Removing_Substrings.java
@@ -0,0 +1,20 @@
+public class _2696_Minimum_String_Length_After_Removing_Substrings {
+    public int minLength(String s) {
+        char[] arr = new char[s.length()];
+        int i = -1;
+        for (char ch : s.toCharArray()) {
+            if (i >= 0 && ((ch == 'B' && arr[i] == 'A') || (ch == 'D' && arr[i] == 'C'))) {
+                i--;
+            } else {
+                arr[++i] = ch;
+            }
+        }
+        return i + 1;
+    }
+
+
+    public static void main(String[] args) {
+        String s = "ABFCACDB";
+
+    }
+}
