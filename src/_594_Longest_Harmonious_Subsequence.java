da1a7aa86a56b5d3d562820f14181fc46960ef4b
Tlee <thienle255@gmail.com>
2025-05-13 22:50:16 +0700

    _594_Longest_Harmonious_Subsequence

--- src/_594_Longest_Harmonious_Subsequence.java
+++ src/_594_Longest_Harmonious_Subsequence.java
@@ -10,13 +10,15 @@ public class _594_Longest_Harmonious_Subsequence {
 
         int longest = 0;
 
-        System.out.println("Tập hợp các value: " + map.values());
-        System.out.println("Tập hợp các key: " + map.keySet());
+//        System.out.println("Tập hợp các key: " + map.keySet());
+//        System.out.println("Tập hợp các value: " + map.values());
 
         for (int key : map.keySet()) {
 
-            System.out.println("key: " + map.get(key));
-            System.out.println("key + 1: " + map.get(key + 1));
+//            System.out.println("longest: " + longest);
+//            System.out.println("key: " + map.get(key));
+//            System.out.println("key + 1: " + map.get(key + 1));
+//            System.out.println("=================================");
             if (map.containsKey(key + 1)) {
                 longest = Math.max(longest, map.get(key) + map.get(key + 1));
             }
