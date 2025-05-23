005df9155af742238068f8cd0f3ae933bdae3f59
Tlee <thienle255@gmail.com>
2025-05-23 23:11:42 +0700

    _744_Find_Smallest_Letter_Greater_Than_Target

---
+++ src/_744_Find_Smallest_Letter_Greater_Than_Target.java
@@ -0,0 +1,26 @@
+public class _744_Find_Smallest_Letter_Greater_Than_Target {
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int left = 0, right = letters.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (letters[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        System.out.println(left % letters.length);
+
+        return letters[left % letters.length];
+    }
+
+    public static void main(String[] args) {
+        char[] letters = {'c', 'f', 'j'};
+        char target = 'a';
+
+        System.out.println(nextGreatestLetter(letters, target));
+
+    }
+}
